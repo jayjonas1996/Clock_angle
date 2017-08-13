34456b4744b873246b1dd6f2bf6052b05885b7cc
Jay Naik <jayjonas1996@outlook.com>
2017-08-13 17:52:55 +0530

    uppercased first character in class name, add new overload toboth methods accept 'DateTime' object. making 3 varient of both methods.

--- Clock angle/Class1.cs
+++ Clock angle/Class1.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;               */
 
 namespace Clock_angle
 {
-    public class clockangle
+    public class Clockangle
     {
 
         //this project features (;) after every comment because equality;
@@ -16,15 +16,16 @@ namespace Clock_angle
 
         /// <summary>
         ///Returns  angle between hour and minute hands of 
-        ///the clock in  Degree and Decimals(DD) format, with hour hand angle being affected by minute hand. current system time is used upon no arguments
+        ///the clock in  Degree and Decimals(DD) format, with hour hand angle being affected by minutes passed. current system time is used upon no arguments
         /// </summary>
-        /// <param name="hh"></param>
-        /// <param name="mm"></param>
+        /// 
         /// <returns></returns>
-        static public float Hands_angle_precise(short hh, short mm)
+        static public float Hands_angle_precise()
         {
-
-            if (hh >= 12)                                            //support for 24 hour format
+            DateTime time = DateTime.Now;
+            short hh = (short)time.Hour;
+            short mm = (short)time.Minute;
+            if (hh >= 12)                                            //support for 24 hour format;
                 hh -= 12;
 
             if (hh > 24 || hh < 0 || mm > 60 || mm < 0)              //invalid input will not be tolerated;
@@ -41,7 +42,95 @@ namespace Clock_angle
 
             //EVERYTHING HAPPENING IN THE FOLLOWING RETURN STATEMENT IS A MASHUP OF ALL THE ABOVE COMMENTED OUT CODE IN TERNARY OPERATOR;
 
-            return Math.Abs(((30f / 5f) * mm) - ((30f * hh) + (.5f * mm))) > 180 ? (360 - Math.Abs(((30f / 5f) * mm) - ((30f * hh) + (.5f * mm)))) : Math.Abs(((30f / 5f) * mm) - ((30f * hh) + (.5f * mm)));                //returns acute angle between two hands, add 180 to the method output or remove code of c** to enable obtuce value;
+            return Math.Abs((6f * mm) - ((30f * hh) + (.5f * mm))) > 180 ? (360 - Math.Abs((6f * mm) - ((30f * hh) + (.5f * mm)))) : Math.Abs((6f * mm) - ((30f * hh) + (.5f * mm)));                //returns acute angle between two hands, add 180 to the method output or remove code of c** to enable obtuce value;
+        }
+
+        /// <summary>
+        ///Returns  angle between hour and minute hands of 
+        ///the clock in  Degree and Decimals(DD) format, with hour hand angle being affected by minute hand. current system time is used upon no arguments
+        /// </summary>
+        /// <param name="hh"></param>
+        /// <param name="mm"></param>
+        /// <returns></returns>
+        static public float Hands_angle_precise(short hh, short mm)
+        {
+
+            if (hh >= 12)                                            //support for 24 hour format;
+                hh -= 12;
+
+            if (hh > 24 || hh < 0 || mm > 60 || mm < 0)              //invalid input will not be tolerated;
+                return -1.0f;
+
+            //float angle;
+
+            //float minute = (30f / 5f) * mm;                          //calculates minute hands angle w.r.t 12 o'clock;
+            //float hour = ((30f * hh) + (.5f * mm));                  //calculates hour angle w.r.t 12 and correction of minute hand is added;
+
+            //angle = (minute - hour);                                 //calculate the difference;
+
+            //angle = Math.Abs(angle);
+
+            //EVERYTHING HAPPENING IN THE FOLLOWING RETURN STATEMENT IS A MASHUP OF ALL THE ABOVE COMMENTED OUT CODE IN TERNARY OPERATOR;
+
+            return Math.Abs((6f * mm) - ((30f * hh) + (.5f * mm))) > 180 ? (360 - Math.Abs((6f * mm) - ((30f * hh) + (.5f * mm)))) : Math.Abs((6f * mm) - ((30f * hh) + (.5f * mm)));                //returns acute angle between two hands, add 180 to the method output or remove code of c** to enable obtuce value;
+        }
+
+        /// <summary>
+        ///Returns  angle between hour and minute hands of 
+        ///the clock in  Degree and Decimals(DD) format, with hour hand angle being affected by minute hand.
+        ///0 will be returned in case of empty DateTime object.
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        static public float Hands_angle_precise(DateTime dt)
+        {
+            short hh = (short)dt.Hour;
+            short mm = (short)dt.Minute;
+            if (hh >= 12)                                            //support for 24 hour format;
+                hh -= 12;
+
+            
+            //float angle;
+
+            //float minute = (30f / 5f) * mm;                          //calculates minute hands angle w.r.t 12 o'clock;
+            //float hour = ((30f * hh) + (.5f * mm));                  //calculates hour angle w.r.t 12 and correction of minute hand is added;
+
+            //angle = (minute - hour);                                 //calculate the difference;
+
+            //angle = Math.Abs(angle);
+
+            //EVERYTHING HAPPENING IN THE FOLLOWING RETURN STATEMENT IS A MASHUP OF ALL THE ABOVE COMMENTED OUT CODE IN TERNARY OPERATOR;
+
+            return Math.Abs((6f * mm) - ((30f * hh) + (.5f * mm))) > 180 ? (360 - Math.Abs((6f * mm) - ((30f * hh) + (.5f * mm)))) : Math.Abs((6f * mm) - ((30f * hh) + (.5f * mm)));                //returns acute angle between two hands, add 180 to the method output or remove code of c** to enable obtuce value;
+        }
+
+        /// <summary>
+        ///Returns  angle between hour and minute hands of 
+        ///the clock in  Degree and Decimals(DD) format, With hour hand angle unaffected by the minutes passed
+        /// </summary>
+        /// 
+        /// <returns></returns>
+        static public float Hands_angle()
+        {
+            DateTime time = DateTime.Now;
+            short hh = (short)time.Hour;
+            short mm = (short)time.Minute;
+            if (hh >= 12)                                            //support for 24 hour format;
+                hh -= 12;
+
+
+            //float angle;
+
+            //float minute = (30f / 5f) * mm;                          //calculates minute hands angle w.r.t 12 o'clock;
+            //float hour = (30f * hh);                                 //calculates hour angle w.r.t 12;
+
+            //angle = (minute - hour);                                 //calculate the difference;
+
+            //angle = Math.Abs(angle);
+
+            //EVERYTHING HAPPENING IN THE FOLLOWING RETURN STATEMENT IS A MASHUP OF ALL THE ABOVE COMMENTED OUT CODE IN TERNARY OPERATOR;
+
+            return Math.Abs((6f * mm) - (30f * hh)) > 180 ? (360 - Math.Abs((6f * mm) - (30f * hh))) : Math.Abs((6f * mm) - (30f * hh));                 //returns acute angle between two hands, add 180 to the method output or remove code of c** to enable obtuce value;
         }
 
         /// <summary>
@@ -54,7 +143,7 @@ namespace Clock_angle
         static public float Hands_angle(short hh, short mm)
         {
 
-            if (hh >= 12)                                            //support for 24 hour format
+            if (hh >= 12)                                            //support for 24 hour format;
                 hh -= 12;
 
             if (hh > 24 || hh < 0 || mm > 60 || mm < 0)              //invalid input will not be tolerated;
@@ -71,56 +160,25 @@ namespace Clock_angle
 
             //EVERYTHING HAPPENING IN THE FOLLOWING RETURN STATEMENT IS A MASHUP OF ALL THE ABOVE COMMENTED OUT CODE IN TERNARY OPERATOR;
 
-            return Math.Abs(((30f / 5f) * mm) - (30f * hh)) > 180 ? (360 - Math.Abs(((30f / 5f) * mm) - (30f * hh))) : Math.Abs(((30f / 5f) * mm) - (30f * hh));                //returns acute angle between two hands, add 180 to the method output or remove code of c** to enable obtuce value;
-        }
-
-        /// <summary>
-        ///Returns  angle between hour and minute hands of 
-        ///the clock in  Degree and Decimals(DD) format, with hour hand angle being affected by minutes passed. current system time is used upon no arguments
-        /// </summary>
-        /// <param name="hh"></param>
-        /// <param name="mm"></param>
-        /// <returns></returns>
-        static public float Hands_angle_precise()
-        {
-            DateTime time = DateTime.Now;
-            short hh = (short)time.Hour;
-            short mm = (short)time.Minute;
-            if (hh >= 12)                                            //support for 24 hour format
-                hh -= 12;
-
-            if (hh > 24 || hh < 0 || mm > 60 || mm < 0)              //invalid input will not be tolerated;
-                return -1.0f;
-
-            //float angle;
-
-            //float minute = (30f / 5f) * mm;                          //calculates minute hands angle w.r.t 12 o'clock;
-            //float hour = ((30f * hh) + (.5f * mm));                  //calculates hour angle w.r.t 12 and correction of minute hand is added;
-
-            //angle = (minute - hour);                                 //calculate the difference;
-
-            //angle = Math.Abs(angle);
-
-            //EVERYTHING HAPPENING IN THE FOLLOWING RETURN STATEMENT IS A MASHUP OF ALL THE ABOVE COMMENTED OUT CODE IN TERNARY OPERATOR;
-
-            return Math.Abs(((30f / 5f) * mm) - ((30f * hh) + (.5f * mm))) > 180 ? (360 - Math.Abs(((30f / 5f) * mm) - ((30f * hh) + (.5f * mm)))) : Math.Abs(((30f / 5f) * mm) - ((30f * hh) + (.5f * mm)));                //returns acute angle between two hands, add 180 to the method output or remove code of c** to enable obtuce value;
+            return Math.Abs((6f * mm) - (30f * hh)) > 180 ? (360 - Math.Abs((6f * mm) - (30f * hh))) : Math.Abs((6f * mm) - (30f * hh));                //returns acute angle between two hands, add 180 to the method output or remove code of c** to enable obtuce value;
         }
 
         /// <summary>
         ///Returns  angle between hour and minute hands of 
         ///the clock in  Degree and Decimals(DD) format, With hour hand angle unaffected by the minutes passed
+        ///0 will be returned in case of empty DateTime object
         /// </summary>
-        /// <param name="hh"></param>
-        /// <param name="mm"></param>
+        /// <param name="dt"></param>
         /// <returns></returns>
-        static public float Hands_angle()
+        static public float Hands_angle(DateTime dt)
         {
-            DateTime time = DateTime.Now;
-            short hh = (short)time.Hour;
-            short mm = (short)time.Minute;
-            if (hh >= 12)                                            //support for 24 hour format
-                hh -= 12;
-
+            
+                short hh = (short)dt.Hour;
+                short mm = (short)dt.Minute;
+                if (hh >= 12)                                            //support for 24 hour format;
+                    hh -= 12;
+                
+            
 
             //float angle;
 
@@ -133,8 +191,10 @@ namespace Clock_angle
 
             //EVERYTHING HAPPENING IN THE FOLLOWING RETURN STATEMENT IS A MASHUP OF ALL THE ABOVE COMMENTED OUT CODE IN TERNARY OPERATOR;
 
-            return Math.Abs(((30f / 5f) * mm) - (30f * hh)) > 180 ? (360 - Math.Abs(((30f / 5f) * mm) - (30f * hh))) : Math.Abs(((30f / 5f) * mm) - (30f * hh));                 //returns acute angle between two hands, add 180 to the method output or remove code of c** to enable obtuce value;
+            return Math.Abs((6f * mm) - (30f * hh)) > 180 ? (360 - Math.Abs((6f * mm) - (30f * hh))) : Math.Abs((6f * mm) - (30f * hh));                 //returns acute angle between two hands, add 180 to the method output or remove code of c** to enable obtuce value;
         }
+
+
 
     }
 }
